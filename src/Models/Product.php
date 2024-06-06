a5e436915a7226455d2594458a3e46fa0070e317
hoan <hoanthinh311@gmail.com>
2024-06-06 20:24:23 +0700

    updta

---
+++ src/Models/Product.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Asus\XuongOop\Models;
+
+use Asus\XuongOop\Commons\Model;
+
+class Product extends Model{
+    protected string $tableName = 'products';
+}
--- src/Models/User.php
+++ src/Models/User.php
@@ -6,4 +6,14 @@ use Asus\XuongOop\Commons\Model;
 class User extends Model 
 {
     protected string $tableName = 'users';
+    public function findByEmail($email)
+    {
+        return $this->queryBuilder
+            ->select('*')
+            ->from($this->tableName)
+            ->where('email = ?')
+            ->setParameter(0, $email)
+            ->fetchAllAssociative(); 
+        
+    }
 }
