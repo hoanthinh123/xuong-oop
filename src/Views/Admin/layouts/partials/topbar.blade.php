8d18d4601f5c17b45c2b78d4dabd8c0c0ae6a64d
hoan <hoanthinh311@gmail.com>
2024-06-08 07:51:01 +0700

    update login

--- src/Views/Admin/layouts/partials/topbar.blade.php
+++ src/Views/Admin/layouts/partials/topbar.blade.php
@@ -124,7 +124,7 @@
                             <div class="profile_info_details">
                                 <a href="#">My Profile </a>
                                 <a href="#">Settings</a>
-                                <a href="#">Log Out </a>
+                                <a href="{{ url('logout') }}">Log Out </a>
                             </div>
                         </div>
                     </div>
